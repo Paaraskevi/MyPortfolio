d880707850625302dc48d54cc10d74fa556ac4be
Paaraskevi <euawhite977@gmail.com>
2024-03-03 12:15:37 +0200

    changes

--- js/header.js
+++ js/header.js
@@ -2,4 +2,6 @@ fetch('header.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('header').innerHTML = html;
-    }); 
+    })
+    .catch(error => console.error('Error fetching header:', error));
+
